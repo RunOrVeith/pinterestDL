81d649ed06d9398721387b843ae53ac91f7a5e85
Veith Roethlingshoefer <veith21@gmail.com>
2017-09-22 23:29:31 +0200

    Add resolution limit

--- download_pinterest.py
+++ download_pinterest.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from datetime import datetime
 from time import sleep
-
+from PIL import Image
 DEBUG = True
 
 
@@ -33,36 +33,58 @@ def find_board_name(board_url):
         name_idx = -2
     return board_url.split("/")[name_idx]
 
-
 def find_high_res_links(body):
     soup = BeautifulSoup(body.get_attribute("outerHTML"), "html.parser")
     low_res_imgs = soup.find_all("img")
     return [img["src"] for img in low_res_imgs], len(low_res_imgs)
 
-
-def download_high_res(high_res_source, download_folder):
+def download_high_res(high_res_source, download_folder, verify_size):
     title = high_res_source.split("--")[-1]
     if not title:
         extension = high_res_source.split(".")[-1]
         title = str(datetime.now()) + f".{extension}"
+    destination = os.path.join(download_folder, title)
     try:
-        urllib.request.urlretrieve(high_res_source, os.path.join(download_folder, title))
+        urllib.request.urlretrieve(high_res_source, destination)
     except urllib.request.ContentTooShortError:
         print(f"Connection died during download of Pin {title}.")
+        return False
 
+    img = Image.open(destination)
+    width, height = img.size
+    # If the image was smaller then we want, we delete it again
+    if not verify_size(width, height):
+        os.remove(destination)
+        return False
+    return True
+
+def get_size_verifier(min_x, min_y, mode):
+    def by_area(width, height):
+        return width * height >= min_x*min_y
+    def by_both(width, height):
+        return width >= min_x and height >= min_y
+    def anything_goes(width, height):
+        return True
+    if mode == "area":
+        return by_area
+    elif mode == "individual":
+        return by_both
+    else:
+        return anything_goes
 
 class PinterestDownloader(object):
 
-    def __init__(self, browser_type="chrome", num_threads=4):
+    def __init__(self, browser_type="chrome", num_threads=4, size_compare_mode=None):
         self.browser = None
         if "chrome" in browser_type:
             self.browser = webdriver.Chrome()
         else:
             raise ValueError("Unsupported browser type")
         self.num_threads = num_threads
+        self.size_compare_mode = size_compare_mode
 
     def load_board(self, board_url, download_folder,
-                   num_pins=None, board_name=None, min_resolution=None,
+                   min_resolution, board_name=None, num_pins=None,
                    skip_tolerance=float('inf')):
         self.browser.get(board_url)
 
@@ -91,10 +113,11 @@ class PinterestDownloader(object):
         else:
             previously_downloaded = []
 
+        print(f"Will download {num_pins} pins from {board_name} to {download_folder}")
+
         # Pick a minimal image resolution
         min_x, min_y = [int(r) for r in min_resolution.split("x")]
-
-        print(f"Will download {num_pins} pins from {board_name} to {download_folder}")
+        size_verifier = get_size_verifier(min_x, min_y, self.size_compare_mode)
 
         # Extract sources of images and download the found ones in parallel
         # Pinterest loads further images with JS, so selenium needs to scroll
@@ -104,16 +127,25 @@ class PinterestDownloader(object):
         downloaded_this_time = []
         nr_previously_downloaded = len(previously_downloaded)
         with concurrent.futures.ThreadPoolExecutor(self.num_threads) as consumers:
-            while len(downloaded_this_time) < num_pins and num_skipped < skip_tolerance:
+            while len(downloaded_this_time) < num_pins and num_skipped < skip_tolerance and num_srcs < num_pins:
                 high_res_srcs, new_num_srcs = find_high_res_links(body)
                 # Submit only the newly found pins to the download queue
                 end_idx = -1 if num_pins > new_num_srcs else num_pins + 1
+                future_to_url = {}
                 for i, high_res_link in enumerate(high_res_srcs[num_srcs:end_idx]):
                     if high_res_link not in previously_downloaded:
-                        future = consumers.submit(download_high_res, high_res_link, download_folder)
-                        downloaded_this_time.append(high_res_link)
+                        future = consumers.submit(download_high_res,
+                                                  high_res_source=high_res_link,
+                                                  download_folder=download_folder,
+                                                  verify_size=size_verifier)
                     else:
                         num_skipped += 1
+                    for fut in concurrent.futures.as_completed(future_to_url):
+                        url = future_to_url[fut]
+                        downloaded = future.result()
+                        if downloaded:
+                            downloaded_this_time.append(url)
+
 
                 # Scroll down if we have not downloaded enough images yet
                 num_srcs = new_num_srcs
@@ -150,6 +182,10 @@ def parse_cmd():
     parser.add_argument("-r", "--resolution", default="0x0", required=False, dest="min_resolution",
                         help="Minimal resolution to download an image. Both dimension must be bigger than the given dimensions. \
                               Input as widthxheight. NOT IMPLEMENTED YET.")
+    parser.add_argument("-m", "--mode", default="individual", required=False, choices=["individual", "area"], dest="mode",
+                        help="Pick how the resolution limit is treated: \
+                        'individual': Both image dimensions must be bigger than the given resolution. \
+                        'area': The area of the image must be bigger than the provided resolution.")
     args = parser.parse_args()
 
     if not os.path.isdir(args.dest_folder):
@@ -159,7 +195,7 @@ def parse_cmd():
 
 if __name__ == "__main__":
     arguments = parse_cmd()
-    dl = PinterestDownloader(num_threads=arguments.nr_threads)
+    dl = PinterestDownloader(num_threads=arguments.nr_threads, size_compare_mode=arguments.mode)
     dl.load_board(board_url=arguments.link, download_folder=arguments.dest_folder,
                   num_pins=arguments.num_pins, board_name=arguments.board_name,
                   min_resolution=arguments.min_resolution)
