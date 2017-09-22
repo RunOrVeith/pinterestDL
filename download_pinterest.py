0a5ec007667c161bcce1a5452ac51e57ae86f85e
Veith Roethlingshoefer <veith21@gmail.com>
2017-09-22 21:52:27 +0200

    Download works in parallel

--- download_pinterest.py
+++ download_pinterest.py
@@ -11,6 +11,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+DEBUG = True
+
+
 
 def find_num_pins(body):
     spans = body.find_elements_by_tag_name("span")
@@ -33,23 +37,13 @@ def find_all_visible_low_res(body):
     return low_res_imgs, len(low_res_imgs)
 
 def download_high_res(high_res_source, img_id, download_folder):
-        extension = source.split(".")[-1]
-        try:
-            urllib.request.urlretrieve(source, os.path.join(download_folder, f"pin_{img_id}.{extension}"))
-        except urllib.request.ContentTooShortError:
-            print(f"Connection died during download of Pin {img_id}.")
+    extension = high_res_source.split(".")[-1]
+    print(f"Going to download {high_res_source} to {download_folder}")
+    try:
+        urllib.request.urlretrieve(high_res_source, os.path.join(download_folder, f"pin_{img_id}.{extension}"))
+    except urllib.request.ContentTooShortError:
+        print(f"Connection died during download of Pin {img_id}.")
 
-def extract_high_res(low_res_link, img_id, download_folder):
-    print("extract high res")
-    fp = urllib.request.urlopen(low_res_link)
-    html = fp.read().decode("utf8")
-    fp.close()
-    print(html)
-        # Maybe need a wait here due to AJAX calls
-        #img = self.browser.find_element_by_tag_name("img")
-        #high_res_source = img.get_attribute("src")
-        #download_high_res(high_res_source, img_id, download_folder)
-        #return high_res_source
 
 class PinterestDownloader(object):
 
@@ -64,7 +58,7 @@ class PinterestDownloader(object):
                    num_pins=None, board_name=None, min_resolution=None,
                    skip_tolerance=float('inf')):
         self.browser.get(board_url)
-        sleep(1) # Let the page load bad style
+        #sleep(1) # Let the page load bad style
 
         body = self.browser.find_element_by_tag_name("body")
         if board_name is None:
@@ -101,24 +95,30 @@ class PinterestDownloader(object):
             num_srcs = 0
             num_skipped = 0
             nr_previously_downloaded = len(previously_downloaded)
-            while num_srcs < num_pins and num_skipped < skip_tolerance:
-                print("Scrolling")
-                self.scroll_down(times=7)
-                low_res_srcs, new_num_srcs = find_all_visible_low_res(body)
 
-                for i, low_res_link in enumerate(low_res_srcs[num_srcs:]):
+            while len(downloaded_this_time) < num_pins and num_skipped < skip_tolerance:
+                low_res_srcs, new_num_srcs = find_all_visible_low_res(body)
+                end_idx = -1 if num_pins > new_num_srcs else num_pins
+                # Submit the newly found pins to the download queue
+                for i, low_res_link in enumerate(low_res_srcs[num_srcs:end_idx]):
                     if low_res_link not in previously_downloaded:
                         print(f"Submitting {low_res_link}")
                         future = consumers.submit(extract_high_res, low_res_link, i + nr_previously_downloaded, download_folder)
-                        #print(future.result(10))
                         downloaded_this_time.append(low_res_link)
                     else:
+                        print("Skipped")
                         num_skipped += 1
-                num_srcs = new_num_srcs
 
-        with open(memory_file_name, 'w+') as f:
-            for source in downloaded_this_time:
-                f.write(f"{source}\n")
+                num_srcs = new_num_srcs
+                if num_srcs < num_pins:
+                    print(f"Scrolling, because {nr_srcs} < {num_pins}")
+                    self.scroll_down(times=7)
+
+
+        if not DEBUG:
+            with open(memory_file_name, 'w+') as f:
+                for source in downloaded_this_time:
+                    f.write(f"{source}\n")
 
 
 
@@ -128,6 +128,19 @@ class PinterestDownloader(object):
             self.browser.execute_script(scroll_js)
             sleep(sleep_time)
 
+def extract_high_res(low_res_link, img_id, download_folder):
+    print("extract high res")
+    fp = urllib.request.urlopen(low_res_link)
+    html = fp.read().decode("utf8")
+    fp.close()
+    soup = BeautifulSoup(html, "html.parser")
+    high_res_source = soup.find("img")["src"]
+    print(high_res_source)
+    download_high_res(high_res_source, img_id, download_folder)
+    return high_res_source
+
+
+
 
 def parse_cmd():
     parser = argparse.ArgumentParser(description='Download a pinterest board or tag page.')
@@ -135,7 +148,7 @@ def parse_cmd():
     parser.add_argument(dest="dest_folder", help="Folder into which the board will be downloaded.")
     parser.add_argument("-n", "--name", default=None, required=False, dest="board_name",
                         help="The name for the folder the board is downloaded in. If not given, will try to extract board name from pinterest.")
-    parser.add_argument("-c", "--count", default=None, required=False, dest="num_pins",
+    parser.add_argument("-c", "--count", default=None, type=int, required=False, dest="num_pins",
                         help="Download only the first 'c' pins found on the page.")
     parser.add_argument("-r", "--resolution", default="0x0", required=False, dest="min_resolution",
                         help="Minimal resolution to download an image. Both dimension must be bigger than the given dimensions. Input as widthxheight.")
