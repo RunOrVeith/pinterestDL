6e47622f54512027232bc165cca85cfba679a721
Veith Roethlingshoefer <veith21@gmail.com>
2017-10-05 12:34:33 +0200

    Improve command line docs

--- pinterestDL/pinterest-dl.py
+++ pinterestDL/pinterest-dl.py
@@ -31,24 +31,33 @@ def parse_cmd():
     # Required arguments
     parser.add_argument(dest="link", help="Link to the pinterest page you want to download.")
     parser.add_argument(dest="dest_folder",
-                        help="""Folder into which the board will be downloaded. Folder with board name is automatically created or found, if it already exists.""")
+                        help="""Folder into which the board will be downloaded.
+                         Folder with board name is automatically created or found inside this folder, if it already exists.
+                         If this folder is named like the page to be downloaded, everything will be directly in this folder.""")
     # Optional arguments
     parser.add_argument("-n", "--name", default=None, required=False, dest="board_name",
-                        help="The name for the folder the board is downloaded in. If not given, will try to extract board name from pinterest.")
+                        help="""The name for the downloaded page. If not given, will try to extract board name from pinterest.
+                        This will also be the name for the folder in which the images are stored.""")
     parser.add_argument("-c", "--count", default=None, type=int, required=False, dest="num_pins",
-                        help="""Download only the first 'c' pins found on the page. If bigger than the number of pins on the board, all pins in the board will be downloaded. The default is to download all pins.""")
+                        help="""Download only the first 'num_pins' pins found on the page.
+                        If bigger than the number of pins on the board, all pins in the board will be downloaded.
+                        The default is to download all pins. If you do not specifiy this option on a tag page, where there are more or less infinite pins,
+                        just stop the script with CTRL+C.""")
     parser.add_argument("-j", "--threads", default=4, type=int, required=False, dest="nr_threads",
-                        help="Number of threads that download images in parallel.")
+                        help="Number of threads that download images in parallel. Defaults to 4.")
     parser.add_argument("-r", "--resolution", default="0x0", required=False, dest="min_resolution",
-                        help="""Minimal resolution to download an image. Both dimension must be bigger than the given dimensions. Input as widthxheight.""")
+                        help="""Minimal resolution for a download image. Input as 'WIDTHxHEIGHT'.""")
     parser.add_argument("-m", "--mode", default="individual", required=False, choices=["individual", "area"], dest="mode",
                         help="""Pick how the resolution limit is treated:
-                             'individual': Both image dimensions must be bigger than the given resolution.
-                             'area': The area of the image must be bigger than the provided resolution.""")
+                             'individual': Both image dimensions must be bigger than the given resolution, i.e x >= WIDTH and y >= HEIGHT.
+                             'area': The area of the image must be bigger than the provided resolution, i.e. x*y >= WIDTH * HEIGHT.""")
     parser.add_argument("-s", "--skip-limit", default=float("inf"), type=int, required=False, dest="skip_limit",
-                        help="""Abort the download after so many pins have been skipped. A pin is skipped if it was already present in the download folder.""")
+                        help="""Abort the download after so many pins have been skipped. A pin is skipped if it was already present in the download folder.
+                            This way you can download new pins that have been added after your last download. Defaults to infinite.
+                            You should not set this to 1, but rather something like 10,
+                            because the page is not scraped exactly in the same order as the pins are added.""")
     parser.add_argument("-t", "--timeout", default=15, type=int, required=False, dest="timeout",
-                        help="Set the timeout after which loading a pinterest board will be aborted, if unsuccessfull.")
+                        help="Set the timeout in seconds after which loading a pinterest board will be aborted, if unsuccessfull. Defaults to 15 seconds.")
     parser.add_argument("-v", "--verbose", default=False, action="store_true", dest="verbose", required=False,
                         help="Display more detailed output and progress reports.")
     args = parser.parse_args()
@@ -64,7 +73,8 @@ if __name__ == "__main__":
         log_level = logging.DEBUG
     logging.basicConfig(level=log_level, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%I:%M:%S')
 
-    with PinterestDownloader(num_threads=arguments.nr_threads,
+    with PinterestDownloader(page_timeout=arguments.timeout,
+                            num_threads=arguments.nr_threads,
                             min_resolution=arguments.min_resolution,
                             size_compare_mode=arguments.mode) as dl:
         dl.download_board(board_url=arguments.link, download_folder=arguments.dest_folder,
